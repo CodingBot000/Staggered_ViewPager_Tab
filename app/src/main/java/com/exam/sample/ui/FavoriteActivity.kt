abaafa25b2298572dfc691bbe21fb7cfeeb6d189
CodingBot000 <codepipe@naver.com>
2020-12-21 18:21:33 +0900

    Alll change ViewPager to ViewPager2

--- app/src/main/java/com/exam/sample/ui/FavoriteActivity.kt
+++ app/src/main/java/com/exam/sample/ui/FavoriteActivity.kt
@@ -2,18 +2,24 @@ package com.exam.sample.ui
 
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.core.view.ViewCompat
 import com.exam.sample.R
-import com.exam.sample.adapter.ViewPagerAdapter
+import com.exam.sample.adapter.ScreenSlideViewPagerAdapter
 import com.exam.sample.common.BaseActivity
 import com.exam.sample.databinding.ActivityFavoriteBinding
 import com.exam.sample.ui.fragment.FavoriteFragment
 import com.exam.sample.ui.fragment.UploadFragment
+import com.exam.sample.utils.DepthPageTransformer
 import com.exam.sample.utils.setImageSize
 import com.exam.sample.viewmodel.FavoriteViewModel
 import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_favorite.*
+import kotlinx.android.synthetic.main.activity_favorite.appbar
+import kotlinx.android.synthetic.main.activity_favorite.pager
 import kotlinx.android.synthetic.main.top_item_favorite.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -28,7 +34,7 @@ class FavoriteActivity :   BaseActivity<ActivityFavoriteBinding, FavoriteViewMod
         get() = R.layout.activity_favorite
 
     override val viewModel : FavoriteViewModel by viewModel()
-    val viewPagerAdapter: ViewPagerAdapter by lazy { ViewPagerAdapter(supportFragmentManager) }
+    val viewPagerAdapter: ScreenSlideViewPagerAdapter by lazy { ScreenSlideViewPagerAdapter(this) }
     private val favoriteFragment = FavoriteFragment()
     private val uploadFragment = UploadFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,12 +48,23 @@ class FavoriteActivity :   BaseActivity<ActivityFavoriteBinding, FavoriteViewMod
         uploadFragment.onNewIntent(intent)
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun init() {
-        viewPagerAdapter.addFragment(favoriteFragment, getString(R.string.tabNameFavorite))
-        viewPagerAdapter.addFragment(uploadFragment, getString(R.string.tabNameUpload))
+        val tabTitles = arrayListOf(R.string.tabNameFavorite, R.string.tabNameUpload)
 
-        tab_layout.setupWithViewPager(pager);
+
+        viewPagerAdapter.addFragment(favoriteFragment)
+        viewPagerAdapter.addFragment(uploadFragment)
+
+
+        pager.setPageTransformer(DepthPageTransformer())
+//        pager.setPageTransformer(ZoomOutPageTransformer())
         pager.adapter = viewPagerAdapter
+
+        TabLayoutMediator(binding.tabLayout, pager) { tab, position ->
+            tab.text = getString(tabTitles[position])
+            pager.setCurrentItem(tab.position, true)
+        }.attach()
 
         var beforeVerticalOffset = 0
         var lastSize = 0
--- app/src/main/java/com/exam/sample/ui/MainActivity.kt
+++ app/src/main/java/com/exam/sample/ui/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.exam.sample.ui
 
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.viewpager2.widget.ViewPager2
+import androidx.annotation.RequiresApi
 
 import com.exam.sample.R
 import com.exam.sample.adapter.ScreenSlideViewPagerAdapter
-import com.exam.sample.adapter.ViewPagerAdapter
 import com.exam.sample.common.BaseActivity
 import com.exam.sample.databinding.ActivityMainBinding
 import com.exam.sample.ui.fragment.*
@@ -18,7 +16,6 @@ import com.exam.sample.viewmodel.MainViewModel
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_trending.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
@@ -31,6 +28,13 @@ class MainActivity :  BaseActivity<ActivityMainBinding, MainViewModel>() {
     override val viewModel : MainViewModel by viewModel()
     val viewPagerAdapter: ScreenSlideViewPagerAdapter by lazy { ScreenSlideViewPagerAdapter(this) }
 
+    private val trendingFragment1 = TrendingFragment()
+    private val trendingFragment2 = TrendingFragment()
+    private val trendingFragment3 = TrendingFragment()
+    private val trendingFragment4 = TrendingFragment()
+    private val trendingFragment5 = TrendingFragment()
+    private val trendingFragment6 = TrendingFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,19 +42,22 @@ class MainActivity :  BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun init() {
         getScreenSize()
 
         val tabTitles = arrayListOf(R.string.tabNameTrending, R.string.tabNameArtists,
             R.string.tabNameClips, R.string.tabNameStories, R.string.tabNameStickers, R.string.tabNameEmoji)
 
-        viewPagerAdapter.addFragment(TrendingFragment())
-        viewPagerAdapter.addFragment(TrendingFragment())
-        viewPagerAdapter.addFragment(TrendingFragment())
-        viewPagerAdapter.addFragment(TrendingFragment())
-        viewPagerAdapter.addFragment(TrendingFragment())
-        viewPagerAdapter.addFragment(TrendingFragment())
+        viewPagerAdapter.addFragment(trendingFragment1)
+        viewPagerAdapter.addFragment(trendingFragment2)
+        viewPagerAdapter.addFragment(trendingFragment3)
+        viewPagerAdapter.addFragment(trendingFragment4)
+        viewPagerAdapter.addFragment(trendingFragment5)
+        viewPagerAdapter.addFragment(trendingFragment6)
 
+//        pager.setPageTransformer(DepthPageTransformer())
+        pager.setPageTransformer(ZoomOutPageTransformer())
         pager.adapter = viewPagerAdapter
 
         TabLayoutMediator(binding.tabLayout, pager) { tab, position ->
@@ -62,7 +69,6 @@ class MainActivity :  BaseActivity<ActivityMainBinding, MainViewModel>() {
       binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 pager.currentItem = tab.position
-
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {}
