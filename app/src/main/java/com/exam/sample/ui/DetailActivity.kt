7ba3b82cc4c334c946cf91e9021abe54ea59c050
CodingBot000 <codepipe@naver.com>
2020-12-29 16:14:46 +0900

    Favorite Msg change Toast to SnackBar

--- app/src/main/java/com/exam/sample/ui/DetailActivity.kt
+++ app/src/main/java/com/exam/sample/ui/DetailActivity.kt
@@ -9,14 +9,17 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.exam.sample.R
 import com.exam.sample.adapter.StaggeredAdapter
 import com.exam.sample.common.BaseActivity
-
 import com.exam.sample.databinding.ActivityDetailBinding
 import com.exam.sample.livedata.EventObserver
 import com.exam.sample.model.data.FavoriteInfo
 import com.exam.sample.model.data.InteractionData
-import com.exam.sample.utils.*
+import com.exam.sample.utils.Const
+import com.exam.sample.utils.Status
 import com.exam.sample.utils.extention.startActivityDetailExtras
+import com.exam.sample.utils.shareUrl
+import com.exam.sample.utils.toastMsg
 import com.exam.sample.viewmodel.DetailViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.detail_middle_view.*
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -68,7 +71,8 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>()
     override fun initObserver() {
         viewModel.apply {
             isLoading.observe(this@DetailActivity, EventObserver {
-                if (it) binding.progress.visibility = View.VISIBLE else binding.progress.visibility =
+                if (it) binding.progress.visibility =
+                    View.VISIBLE else binding.progress.visibility =
                     View.GONE
             })
 
@@ -107,6 +111,7 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>()
         viewModel.apply {
             getDetailData(interactionData.userId)
         }
+
     }
 
     private fun initClickEvent() {
@@ -115,6 +120,9 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>()
         }
 
         ll_middleView.apply {
+            // 최초 getFavorite을 통해서 결과를 받아와서 좋아요 체크를 해주는데 이때 아래 changeListener가 불림.
+            // 어차피 insert가     @Insert(onConflict = REPLACE) 이기 때문에
+            // 그냥 두었음. 만약 필요하다면 boolean변수로 제어하던가, 최초에 setOnCheckedChangeListener를 null로 해제했다 재등록하면됨
             checkBoxFavorite.setOnCheckedChangeListener { compoundButton, b ->
                 if (b) {
                     viewModel.insertFavorite(
@@ -124,7 +132,8 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>()
                             interactionData.type
                         )
                     )
-                    toastMsg(R.string.favorite_check)
+
+                    makeSnackBar(R.string.favorite_check, R.string.favorite_uncheck)
                 }
                 else {
                     viewModel.removeFavorite(
@@ -134,7 +143,7 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>()
                             interactionData.type
                         )
                     )
-                    toastMsg(R.string.favorite_uncheck)
+                    makeSnackBar(R.string.favorite_uncheck, R.string.favorite_check)
                 }
             }
 
@@ -147,6 +156,19 @@ class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>()
                 toastMsg(R.string.msgMore)
             }
         }
+    }
+
+    private fun makeSnackBar(msgResId: Int, btnNameResId:Int) {
+        val snackbar = Snackbar.make(
+            topBar,
+            msgResId,
+            Snackbar.LENGTH_SHORT
+        )
+        snackbar.setAction(getString(btnNameResId)) {
+            checkBoxFavorite.performClick()
+            snackbar.dismiss()
+        }
+        snackbar.show()
     }
 
     override fun onDestroy() {
