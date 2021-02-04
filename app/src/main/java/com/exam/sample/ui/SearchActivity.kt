3da6f65ef31895bc7bd1548e4f2b2ba6010e930d
CodingBot000 <codepipe@naver.com>
2021-02-04 23:49:07 +0900

    big fix
    SearchKeyTextWatcher innser Class를 독립 class로 분리
    activity_detail.xml 커밋누락된것 커밋

--- app/src/main/java/com/exam/sample/ui/SearchActivity.kt
+++ app/src/main/java/com/exam/sample/ui/SearchActivity.kt
@@ -174,29 +174,6 @@ class SearchActivity : BaseActivity<ActivitySearchBinding, SearchViewModel>()
         edtSearch.requestFocus()
     }
 
-    inner class SearchKeyTextWatcher: TextWatcher {
-        private val textChangeSubject = PublishSubject.create<String>()
-
-        init {
-            textChangeSubject.onNext("")
-        }
-
-        override fun afterTextChanged(s: Editable?) {
-            if (s.isNullOrEmpty())
-                textChangeSubject.onNext("")
-            else
-                textChangeSubject.onNext(s.toString())
-        }
-
-        override fun beforeTextChanged(p0: CharSequence?, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {}
-
-        fun getSearchKeyChangeObserver(): Observable<String> {
-            return textChangeSubject.debounce(Const.EMIT_INTERVAL, TimeUnit.MILLISECONDS)
-                .distinctUntilChanged()
-        }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
 
